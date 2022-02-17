85776c7a61699e6906d191caa83e7c1a31dcd49a
Your Name <you@example.com>
2022-02-17 11:28:34 -0500

    Commit 2do

---
+++ PruebaTecnica/inventario/choices.py
@@ -0,0 +1,31 @@
+tipoSeccion=(
+    ('1', 'Tecnología'),
+    ('2', 'Industrial'),    
+    ('3', 'Hogar'),    
+    ('4', 'Víveres'),
+    ('5', 'Niños'),
+    ('6', 'Mascotas'),    
+
+)
+
+ubicacion=(
+
+    ('1', 'Norte'),
+    ('2', 'Sur'),
+    ('3', 'Oriente'),
+    ('4', 'Occidente'),
+    ('5', 'NorOriente'),
+    ('6', 'NorOccidete'),
+    ('7', 'SurOriente'),
+    ('8', 'SurOccidente'),
+
+)
+
+
+cargo=(
+    ('1', 'Gerente'),
+    ('2', 'Administrador'),
+    ('3', 'Empleado'),
+
+
+)
--- PruebaTecnica/inventario/models.py
+++ PruebaTecnica/inventario/models.py
@@ -1,5 +1,28 @@
 from pyexpat import model
 from django.db import models
+from .choices import tipoSeccion, ubicacion, cargo
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=100, choices=tipoSeccion  ,null=True, verbose_name='Nombre')
+    ubicacion = models.CharField(max_length=100, choices=ubicacion  , null=True, verbose_name='Ubicación')
+    descripcion = models.CharField(max_length=100, null=True, verbose_name='Descripción')
+
+    def seccion(self):
+        return "{}, {}".format(self.nombre, self.ubicacion)
+
+    def __str_(self):
+        return self.seccion()
+
+class Responsable(models.Model):
+    nombre = models.CharField(max_length=100, null=True, verbose_name='Nombre')
+    apellidos = models.CharField(max_length=100, null=True, verbose_name='Apellidos')
+    cargo = models.CharField(max_length=100, choices=cargo  , null=True, verbose_name='Cargo')
+
+    def responsable(self):
+        return "{}, {}, {}".format(self.nombre, self.apellidos, self.cargo)
+
+    def __str__(self):
+        return self.responsable()
 
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,7 +32,11 @@ class Producto(models.Model):
     valor = models.FloatField(max_length=100, null=True, verbose_name='Valor')
     fecha = models.DateField(max_length=100, null=True, verbose_name='Fecha')
     estado = models.BooleanField(max_length=100, null=True, verbose_name='Estado')
+    seccion = models.ForeignKey(Seccion, null=True, on_delete=models.DO_NOTHING )
+    cargo = models.ForeignKey(Responsable, null=True, on_delete=models.DO_NOTHING )
 
     def __str__(self):
         fila='Nombre: ' + self.nombre + "  -   " + 'Descripción: ' + self.descripcion
         return fila
+
+
